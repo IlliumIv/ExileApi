3f1c30cc85bab5b96be7f795997646167a05ab33
IlliumIv <alm.sparta.ao@gmail.com>
2020-09-20 15:57:22 +0500

    Submodules update.

--- create_archive.bat
+++ create_archive.bat
@@ -1,5 +1,9 @@
 cd ..\PoeHelper
+del ExileCore.dll
+del GameOffsets.dll
 del PoeHelper.7z
 del PoeHelper.7z.*
 "C:\Program Files\7-Zip\7z.exe" u -x@..\ExileApi\archive_exclude.txt PoeHelper.7z *
+copy ..\ExileApi\ExileCore.dll ExileCore.dll
+copy ..\ExileApi\GameOffsets.dll GameOffsets.dll
 exit
